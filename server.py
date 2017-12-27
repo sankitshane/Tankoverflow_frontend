824cf8c7758c1a93eaa9f4e470210d1bae53453a
sankit acharya <sankit.acharya@gmail.com>
2017-12-27 20:56:40 +0530

    chore: Added flask as the server
    Removed node and added flask as the server, as a common platform
    between backend and frontend

---
+++ server.py
@@ -0,0 +1,13 @@
+import json
+import os
+import time
+from flask import Flask, Response, request, render_template
+from nocache import nocache
+
+app = Flask(__name__, static_url_path='', static_folder='app/dist')
+app.add_url_rule('/', 'root', lambda: app.send_static_file('view/index.html'))
+
+
+
+if __name__ == '__main__':
+    app.run(port=int(os.environ.get("PORT", 8000)), debug=True)
