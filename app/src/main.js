797efe961c81bc915170ce7d0b106e8923b72a14
sankit acharya <sankit.acharya@gmail.com>
2018-01-08 22:08:32 +0530

    fect: Created header layout
    Created a demo head layout to get started on the app.

--- app/src/main.js
+++ app/src/main.js
@@ -1,7 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+class Header extends React.Component {
+  constructor(prop){
+    super(prop);
+    this.state = {}
+  }
+
+  render() {
+    return(
+      <div className="demo-layout-transparent mdl-layout mdl-js-layout">
+        <header className="mdl-layout__header mdl-layout__header--transparent">
+          <div className="mdl-layout__header-row">
+            <span className="mdl-layout-title">Tank Over Flow</span>
+            <div className="mdl-layout-spacer"></div>
+            <nav className="mdl-navigation">
+              <a className="mdl-navigation__link" href="">Login</a>
+            </nav>
+          </div>
+        </header>
+        <main className="mdl-layout__content">
+        </main>
+      </div>
+    );
+  }
+}
+
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <Header />,
   document.getElementById('root')
 );
